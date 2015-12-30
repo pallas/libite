24e15fb9fbaeeb7b06ff8b8cb751946ecab43183
Derrick Pallas <derrick@pallas.us>
2015-12-30 13:08:30 -0800

    link: add one-bit tags
    For pointers to aligned memory, we can stuff extra information into the low
    address bits, which should normally always be zero.  Packing a bool into
    this space can save significant memory, e.g. the color bit in a red-black
    tree.  Previously, this was implemented in intrusive_tree_link directly but
    has wider applications.

--- intrusive_link.h
+++ intrusive_link.h
@@ -3,6 +3,7 @@
 
 #include <cassert>
 #include <cstddef>
+#include <stdint.h>
 
 template <class T>
 struct intrusive_link {
@@ -10,6 +11,17 @@ struct intrusive_link {
   ~intrusive_link() { assert(!p); }
 
   T* p;
+};
+
+template <class T, intptr_t TAG = 0x1>
+struct intrusive_link_tag : public intrusive_link<T> {
+  typedef intrusive_link<T> link;
+
+  void tag(T* t) { link::p = (T*)(intptr_t(t) | TAG); }
+  void toggle() { link::p = (T*)(intptr_t(link::p) ^ TAG); }
+
+  bool tagged() const { return intptr_t(link::p) & TAG; }
+  T* tagless() const { return (T*)(intptr_t(link::p) & ~TAG); }
 };
 
 #endif//INTRUSIVE_LINK_H
