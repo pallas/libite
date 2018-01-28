a292b7d3f2fedfb4b743851e47407d71b0d02686
Derrick Pallas <derrick@pallas.us>
2018-01-27 21:40:34 -0800

    test: output original, random queue

--- test_queue.cc
+++ test_queue.cc
@@ -10,7 +10,7 @@ struct node {
   int value;
   node(int v) : value(v) { }
 
-  lite::queue_link<node> forward_link, reverse_link;
+  lite::queue_link<node> random_link, forward_link, reverse_link;
 
   bool
   bound() const {
@@ -22,6 +22,7 @@ struct node {
 
   void kill() { if (!bound()) delete this; }
 
+  typedef lite::queue<node, &node::random_link> random_queue_t;
   typedef lite::queue<node, &node::forward_link> forward_queue_t;
   typedef lite::queue<node, &node::reverse_link> reverse_queue_t;
 
@@ -33,6 +34,7 @@ int
 main(int, char*[]) {
   srand48(getpid());
 
+  node::random_queue_t random;
   node::forward_queue_t forward;
   node::reverse_queue_t reverse;
 
@@ -40,6 +42,7 @@ main(int, char*[]) {
 
   for (unsigned i = 0 ; i < n ; ++i) {
     node* x = new node(lrand48() % n);
+    random.enqueue(x);
     forward.enqueue(x);
   }
 
@@ -52,6 +55,14 @@ main(int, char*[]) {
   assert(node::reverse_sorter_t::sorted(reverse));
   assert(forward.peek()->value == reverse.last()->value);
   assert(forward.last()->value == reverse.peek()->value);
+
+  std::cout << "random" << '\t';
+  while (!random.empty()) {
+    node* x = random.dequeue();
+    std::cout << ' ' << x->value;
+    x->kill();
+  }
+  std::cout << std::endl;
 
   std::cout << "forward" << '\t';
   while (!forward.empty()) {
